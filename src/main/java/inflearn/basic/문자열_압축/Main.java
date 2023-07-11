293cbcc891ed1f987f4fd01b0c808b284e43d680
Min Kyu Kim <devsmile@twc.co.kr>
2023-07-11 18:22:12 +0900

    basic algorithms code updated-2

--- src/main/java/inflearn/basic/문자열_압축/Main.java
+++ src/main/java/inflearn/basic/문자열_압축/Main.java
@@ -4,13 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public String solution(String str) {
+        String answer = "";
+        char beforeChar = '+'; int cnt =1;
+        for (char c : str.toCharArray()) {
 
+            if (c != beforeChar) {
+                beforeChar = c;
+                if (cnt > 1){
+                    answer += cnt;
+                }
+                cnt = 1;
+                answer += c;
+            }else {
+                cnt ++;
+            }
+        }
+        if (cnt > 1){
+            answer += cnt;
+        }
+        return answer;
     }
     public static void main(String[] args) {
         Main m = new Main();
         Scanner in = new Scanner(System.in);
-        String string ="";
-        string=in.next();
+        String string ="KSTTTSEEKFKKKDJJGG";
+//        string=in.next();
         System.out.println(m.solution(string));
     }
 }
--- src/main/java/inflearn/basic/암호/Main.java
+++ src/main/java/inflearn/basic/암호/Main.java
@@ -1,2 +1,27 @@
-package inflearn.basic.암호;public class Main {
+package inflearn.basic.암호;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+
+public class Main {
+    public String solution(String str, int characterNum) {
+        String answer = "";
+        String s = str.replaceAll("#", "1").replaceAll("\\*", "0");
+        char[] chars = new char[characterNum];
+        for (int i = 0; i < characterNum; i++ ){
+            String s1 = s.substring(i * 7, (i + 1) * 7);
+            System.out.print((char) Integer.parseInt(s1, 2));
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner in = new Scanner(System.in);
+        int characterNum = 4;
+        String string = "#****###**#####**#####**##**";
+//        string=in.next();
+        System.out.println(m.solution(string, characterNum));
+    }
 }
