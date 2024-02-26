4d1079f0fe0f27be6a6b09082537ba69f6f0372f
김민규 <kim.mingyu3@joins.com>
2024-02-26 09:31:23 +0900

    .

--- src/main/java/re2024/baekjoon/dfs/바이러스/Virus3.java
+++ src/main/java/re2024/baekjoon/dfs/바이러스/Virus3.java
@@ -1,62 +1,56 @@
 package re2024.baekjoon.dfs.바이러스;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Virus3 {
-    static ArrayList<Integer>[] graph;
+    static ArrayList<Integer>[]  graph;
     static boolean [] visited;
     static int n, m;
     static int answer = 0;
-    static void dfs(int num){
-        visited[num] = true;
+
+    public void dfs(int x){
+        visited[x] = true;  
         answer++;
-        for (int i = 0; i < graph[num].size(); i++) {
-            if(!visited[graph[num].get(i)]){
-                dfs(graph[num].get(i));
+
+        for(int i = 1 ; i < graph[x].size(); i ++){
+            if(!visited[graph[x].get(i)] ){
+                dfs(graph[x].get(i));
             }
         }
-    }
 
-    public static void main(String[] args) throws IOException{
+    } 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
-        
-        graph = new ArrayList[100];
+        graph = new ArrayList[n+1]; // Fix: Specify the type parameter
         visited = new boolean[n+1];
-
-
-        int x = 0;
-        int y = 0;
-        for (int i = 1; i <=n; i++) {
-            graph[i] = new ArrayList<>();
-            
-        }
         
-        for (int i = 1; i<= m ; i ++) {
+        for(int i = 0 ; i< n+1; i++){
+            graph[i] = new ArrayList<Integer>();
+        }
+
+
+        int x, y = 0;
+
+        for (int i = 0 ; i < m ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             x = Integer.parseInt(st.nextToken());
             y = Integer.parseInt(st.nextToken());
             graph[x].add(y);
             graph[y].add(x);
         }
+        Virus3 v = new Virus3();
+        v.dfs(1);
 
-        for( int i = 1 ; i <= n ; i++ ){
-            Collections.sort(graph[i]);
-        } // 정렬
-
-        dfs(1);
-
-        for (boolean v : visited) {
-            System.out.println(v);
-        }
         System.out.println(answer-1);
+        br.close(); 
+
     }
-    
 }
---
+++ src/main/java/re2024/baekjoon/dfs/연결요소의개수/LinkFactorNumber3.java
@@ -0,0 +1,68 @@
+package re2024.baekjoon.dfs.연결요소의개수;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class LinkFactorNumber3 {
+    final static int MAX = 1000 + 10;
+    static ArrayList<Integer>[]  graph;
+    static boolean [] visited;
+    static int n, m;
+    static int answer = 0;
+
+    void dfs(int x) {
+        visited[x] = true;
+        
+        for(int i = 1; i <= graph[x].size() ; i ++) {
+            System.out.println(i);
+            if(!visited[graph[x].get(i)])
+                dfs(graph[x].get(i));
+        }
+        
+    }
+    /**
+     * 2024.02.21
+     * 
+     * 백준 DFS - 연결요소의 갯수
+     * 백준 11724번 문제입니다.
+     */
+
+    public static void main(String[] args) throws IOException {
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+ 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        graph = new ArrayList<Integer>[n+1]; 
+        for(int i = 0 ; i< n+1; i++){
+            graph[i] = new ArrayList<Integer>();
+        }
+        visited = new boolean[MAX];
+
+        int x, y;
+        // 1. 그래프 생성
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            
+            graph[x].add(y);
+            graph[y].add(x);
+
+        }
+        LinkFactorNumber3 ln = new LinkFactorNumber3();
+
+        for(int i=1 ; i <= n ;i ++){
+            if(!visited[i]){
+                System.out.println(i);
+                ln.dfs(i);
+                answer++;
+            }
+
+        }
+        // System.out.println(answer);
+    }
+}
